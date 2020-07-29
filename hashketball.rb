c7b9c74344d89303434c475a455f0187d929fa31
Nunnyr <nunnyreyes@gmail.com>
2020-07-29 13:52:13 -0400

    my  brain

--- hashketball.rb
+++ hashketball.rb
@@ -248,3 +248,138 @@ def player_numbers(string)
   # end
 
 end
+
+
+
+#########################################################################
+#######review with ERIC#################
+#if you can't find pry then you would do gem install pry
+#at the top you would require 'pry'. This is the act of getting this tool
+#anytime that you have a binding.pry in your method definition it will stop the code. 
+#but it does not invoke it if its a method you wrote on your own to test
+#When I run rspec line 1 gets read which is what triggers the binding.pry to be hit
+#sometimes binding.pry at the VERY end of your file may not work.
+#sometimes you can put a 0 or something at the end so that it can get hit
+
+#for all of the players that are in the league you want an array of hashes
+#[{}, {}, {}, {}, {}]
+#do not assume that your hash will always be structured with 2 teams inside of it
+
+
+def get_me_all_players_please
+ #[{}, {}, {}, {}, {}]
+ #use enumerables to iterate
+ 
+ all_players = []
+ game_hash.each do |home_or_away_sym, team_info_hash|
+  #if you want to get behind the value of a key put it behind brackets
+    all_players += team_info_hash[:players]
+    #in RUBY you can add arrays [1,2,3] + [4,5,6] ===== [1,2,3,4,5,6]
+  binding.pry
+ end
+ all_players
+end
+
+
+
+def big_shoe_rebounds
+##method called maxby that expects the block to be a number
+
+    #get_me_all_players.max_by
+    #get_me_all_players.min_by 
+
+theInfoAboutThePlayerWithBigShoes = get_me_all_players.max_by do |player_hash|
+      player_hash[:shoe]
+    end
+    #it gives me the element in the array that has the biggest something 
+    theInfoAboutThePlayerWithBigShoes[:rebounds]
+end
+
+binding.pry
+
+
+
+
+
+
+
+
+def find_a_single_team_based_on_name(team_name_arg_str)
+
+  game_hash.find do |home_or_away_sym, team_info_hash|
+    team_info_hash[:team_name] == team_name_arg_str
+    #the return of find is always going to be an array of length 2
+    binding.pry
+end
+
+find_a_single_team_based_on_name(team_name_arg_str) #now you know for a fact I am invoking this number
+
+
+def team_colors(arg_team_name)
+  infoAboutAsingleTeam = find_a_single_team_based_on_name(arg_team_name)
+  infoAboutASingleTeam[1][:colors]
+  #the above is the implicit returnand find will always return an array of 2
+  #and you can uise the index to find a color 
+  #if you dont know the index THEN its okay to write an if statement to check if its the index that you are at
+
+  binding.pry
+end
+#method definition means you need to invoke the method. parenthese are optional. i prefer it.
+#whereami - is there is the scope
+# v.class will tell you what things are in
+# v.keys 
+#.each returns the hash that you are iterating over
+#now we have a single method whose job is to get us all of the players
+get_me_all_players_please() #this returns an array of hashes
+
+
+#we want to use a map bc it will return a new array
+#we want to go through an array of 5 hashes and make it one array
+#if we are transforming the elemtns map is your best friend
+
+def player_numbers(teamName)
+  infoABoutASingleTeam = find_a_single_team_based_on_name(arg_team_name)
+  players_on_a_team = = infoABoutASingleTeam[1][:players]
+  players_on_a_team.map do |player_hash
+
+    player_hash[:number]
+  end
+end
+
+def team_names
+  game_hash.map do |home_or_away_sym, team_info_hash|
+    team_info_hash[:team_name]
+  end
+end 
+#map transforms key values into strings.
+
+
+####################FIND
+#this method returns an array of hashes
+#you don't need an if statement because it is comparing what it found to the argument passed originally in the definition
+
+
+def player_stats(player_name_arg)
+  get_me_all_players_please().find do |player_hash|
+  player_hash[:player_name] == player_name_arg
+    #binding.pry
+  end
+end 
+
+
+
+def num_points_scored(player_name_argument)
+  found_player_hash = player_stats(player_name_argument)
+  found_player_hash[:points]
+  #player_stats here is a helper method
+
+end
+
+
+def shoe_size(player_name__a)
+  player_stats(player_name_a)[:shoe]
+end 
+#############You can chain methods together
+
+
+
